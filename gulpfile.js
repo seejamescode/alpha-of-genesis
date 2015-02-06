2771929752053b90a382e324163da85f73d8bd7c
James Rauhut <jamesrauhut@gmail.com>
2015-02-06 11:49:09 -0600

    Project now localhosts on gulp and particles changed to image

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var connect = require('gulp-connect');
 
 gulp.task('html', function() {
     return gulp.src('html/*.html')
         .pipe(gulp.dest('static'))
+});
+
+gulp.task('images', function() {
+    return gulp.src('images/**')
+        .pipe(gulp.dest('static/images'))
 });
 
 /* Compile Our Sass */
@@ -42,11 +48,19 @@ gulp.task('component_css', function() {
         .pipe(gulp.dest('static/css'));
 });
 
+gulp.task('webserver', function() {
+    connect.server({
+        port: 5000,
+        root: 'static'
+    });
+});
+
 /* Watch Files For Changes */
 gulp.task('watch', function() {
     gulp.watch('html/*.html', ['html']);
+    gulp.watch('images/**', ['images']);
     gulp.watch('scss/*.scss', ['sass']);
     gulp.watch('js/*.js', ['js']);
 });
 
-gulp.task('default', ['html', 'sass', 'js', 'component_js', 'component_css', 'watch']);
+gulp.task('default', ['html', 'images', 'sass', 'js', 'component_js', 'component_css', 'watch', 'webserver']);
