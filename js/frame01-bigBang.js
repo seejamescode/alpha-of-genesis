6b753bbddb783a63d9d874af2c095346affd35cb
James Rauhut <jamesrauhut@gmail.com>
2015-02-22 23:47:34 -0600

    Modified big bang variables for explosion effect

--- js/frame01-bigBang.js
+++ js/frame01-bigBang.js
@@ -1,20 +1,56 @@
 // variables used in init()
 var scene, camera, renderer, stats, stats2, clock;
+var particleEmitter, particleGroup
+var mouseX = 0, mouseY = 0;
 
-// Used in initParticles()
-var emitter, particleGroup, pool,
-    pos = new THREE.Vector3(),
-        emitterSettings = {
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10000);
+    camera.position.z = 50;
+    camera.lookAt( scene.position );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor(0x000000);
+
+    stats = new Stats();
+    clock = new THREE.Clock();
+
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0';
+
+    document.body.appendChild( renderer.domElement );
+    document.body.appendChild( stats.domElement );
+}
+
+function initParticles() {
+    particleGroup = new SPE.Group({
+        texture: THREE.ImageUtils.loadTexture('images/particle.png'),
+        maxAge: 60,
+        hasPerspective: 1,
+        colorize: 0,
+        transparent: 1,
+        alphaTest: 0.5,
+        depthWrite: false,
+        depthTest: true,
+        blending: THREE.AdditiveBlending
+    });
+
+    // Used in initParticles()
+    particleEmitter = new SPE.Emitter({
         type: 'sphere',
-        particleCount: 500,
+        particleCount: 1000,
         position: new THREE.Vector3( 0, 0, 0 ),
         positionSpread: new THREE.Vector3( 0, 0, 0 ),
         radius: 0.001,
         radiusSpread: 0,
         radiusSpreadClamp: 0,
         radiusScale: new THREE.Vector3( 1, 1, 1 ),
-        speed: 1,
-        speedSpread: 5,
+        speed: 10,
+        speedSpread: -20,
         sizeStart: 1,
         sizeStartSpread: 0,
         sizeMiddle: 1,
@@ -40,75 +76,26 @@ var emitter, particleGroup, pool,
         opacityMiddleSpread: 0,
         opacityEnd: 1,
         opacityEndSpread: 0,
-        duration: 0.5,
+        duration: 0.01,
         alive: 1,
         isStatic: 0
-    };
+    });
 
-var mouseX = 0, mouseY = 0;
-
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-
-// Setup the scene
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10000);
-    camera.position.z = 50;
-    camera.lookAt( scene.position );
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setClearColor(0x000000);
-
-    stats = new Stats();
-    clock = new THREE.Clock();
-
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.top = '0';
-
-    document.body.appendChild( renderer.domElement );
-    document.body.appendChild( stats.domElement );
-}
-
-// Create particle group and emitter
-function initParticles() {
-    particleGroup = new SPE.Group({
-                    texture: THREE.ImageUtils.loadTexture('images/particle.png'),
-                    maxAge: 60,
-                    hasPerspective: 1,
-                    colorize: 0,
-                    transparent: 1,
-                    alphaTest: 0.5,
-                    depthWrite: false,
-                    depthTest: true,
-                    blending: THREE.AdditiveBlending
-                });
-
-    particleGroup.addPool( 1, emitterSettings, false );
+    // Add the emitter to the group.
+    particleGroup.addEmitter( particleEmitter );
 
     // Add particle group to scene.
     scene.add( particleGroup.mesh );
 }
 
-// Generate a random number between -size/2 and +size/2
-function rand( size ) {
-    return size * Math.random() - (size/2);
-}
-// Trigger an explosion
-function createExplosion() {
-    var num = 1000;
-    particleGroup.triggerPoolEmitter( 1, (0, 0, 0) );
-}
-
 function animate() {
     requestAnimationFrame( animate );
 
-    // Using a fixed time-step here to avoid pauses
-    render();
+    // Use a fixed time-step here to avoid gaps
+    render( clock.getDelta() );
+
     stats.update();
 }
-
 
 function render( dt ) {
     particleGroup.tick( dt );
@@ -171,6 +158,3 @@ init();
 initParticles();
 
 setTimeout(animate, 0);
-
-// Add a mousedown listener. When mouse is clicked, a new explosion will be created.
-setTimeout(function () {createExplosion()}, 1000);
