db64a3c43f51a89048422cd300fdf571a6112024
James Rauhut <jamesrauhut@gmail.com>
2015-03-08 22:01:32 -0500

    Fixed frame03 element widths and took resource out of js file

--- js/steps.js
+++ js/steps.js
@@ -1,9 +1,5 @@
 var step = { current: 0};
 
-function step02() {
-    console.log("WE MADE IT TO STEP 2");
-}
-
 watch(step, "current", function() {
     console.log("Changed step to " + step.current);
     switch(step.current) {
@@ -15,9 +11,9 @@ watch(step, "current", function() {
             $('#frame02').css("opacity", "1");
             $('canvas').first().remove();
             setTimeout( function() {
+                $('#frame01').css("display", "none");
                 $('#frame02 .glitch').css("opacity", "1");
             }, 200);
-            step02();
             $( "#frame02 .glitch" ).on('click touchstart', function () {
                 step.current = 3;
             });            
@@ -27,6 +23,7 @@ watch(step, "current", function() {
             step03();
             $('#frame02 .glitch').css("opacity", "0");
             setTimeout( function() {
+                $('#frame02').css("display", "none");
                 $('#frame03').css("opacity", "1");
             }, 500);
             $( "#frame03 .glitch" ).on('click touchstart', function () {
@@ -34,15 +31,33 @@ watch(step, "current", function() {
             });  
             break;
         case 4:
-            step04();
-            $('#sun-glow').css("left", "0");
+            step04(2);
+            $('#sun-container #sun-glow').css("left", "10%");
             $('#frame03 > .glitch-box > .glitch').css("opacity", "0");
             setTimeout( function() {
-                $('#frame03 > .glitch-box').css("Display", "None");
+                $('#frame03 > .glitch-box').css("display", "done");
             }, 250);
-            $('#earth-container').css("left", "32.5%");
+            $('#earth').css("left", "32.5%");
+            $( "#sun-container #sun-glow .glitch" ).on('click touchstart', function () {
+                step.current = 5;
+            }); 
+            break;
+        case 5:
+            step04(3);
+            $('#sun-container #sun-glow').addClass("sun-fast");
+            $('#sun-container #sun-glow').removeClass("sun-slow");
+            $('#earth').css("left", "67.5%");
+            $('#sun-container #sun-glow').css("left", "-100%");
+            setTimeout( function() {
+                $('#sun-container2 #sun-glow').css("left", "2.5%");
+                $('#sun-container').css("display", "none");
+                setTimeout( function() {
+                    $('#step05').css("display", "initial");
+                    $('#step05 .glitch').css("opacity", "1");
+                }, 3500);
+            }, 3500);
             break;
     };
 });
 
-step.current = 3;
+step.current = 1;
